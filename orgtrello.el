0e3155783ee158d2123dafd6b9048a80f4b4026f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-29 20:41:05 +0200

    Adding the clj-thrush.el lib which reclaim clojure's threading macro (missed them so much!)

--- orgtrello.el
+++ orgtrello.el
@@ -93,11 +93,11 @@
 ;;  (id . "51a326c424aa585670000180"))
 
 ;; ELISP> (->> boards
-;;      (mapcar
-;;       (lambda (board)
-;;         (->> board
-;;              (assq 'id)
-;;              cdr))))
+;;             (mapcar
+;;              (lambda (board)
+;;                (->> board
+;;                     (assq 'id)
+;;                     cdr))))
 ;; ("50aa59502ddab2fc1100115b" "4f96a984dbb00d733b04d8b5" "518815ca40491e9a7b006f21" "51626230b314a2252c0007eb" "50ceefed35a573c76e001852" "51a327c85436a5ee3e0045b4" "4f2baa3072b7c1293501caea" "4f2ba2054f2cb9d16d3f7f99" "50bcfd2f033110476000e768" "50169deffa899b184693ee76" "51a326c424aa585670000180")
 
 ;;(plist-get id boards)
