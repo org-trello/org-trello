286549bcc2ba38c376374657f04c53cd1b3951d3
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-03 21:54:51 +0200

    Adding missed dependency.

--- org-trello-pkg-template.el
+++ org-trello-pkg-template.el
@@ -1,5 +1,6 @@
 (define-package "orgtrello" ###VERSION### "org-mode minor mode to synchronize org-mode file and a trello board [source: https://github.com/ardumont/org-trello]"
   '((org     "7.9.2")
+    (cl-lib  "0.3.0")
     (json    "1.3")
     (dash    "20130617.739")
     (request "20130526.1915")))
