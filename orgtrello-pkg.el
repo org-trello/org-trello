a1875d055049da41bb0bfbaae5c293ca751d08c9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-03 18:40:30 +0200

    Remove orgtrello-pkg.el as this will be generated by the make.

--- orgtrello-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "orgtrello" "20130703.1720" "org-mode minor mode to synchronize org-mode file and a trello board [source: github]"
-  '((org     "7.9.2")
-    (json    "1.3")
-    (dash    "20130617.739")
-    ;; check for emacs-request from package list
-    (request "20130526.1915")))
