c270500cd62fa835c408e6aa8d55064b4b562d85
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-06 11:18:20 +0200

    It's more standard to use the official release (https://github.com/tkf/emacs-request/tags and https://github.com/magnars/dash.el/tags)

--- org-trello-pkg.el
+++ org-trello-pkg.el
@@ -1,3 +1,3 @@
 (define-package "org-trello" "0.0.1"
   "Minor mode for org-mode to sync org-mode and trello"
-  '((org "7.9.2") (dash "20130617.739") (request "20130526.1915") (cl-lib "0.3.0") (json "1.3")))
+  '((org "7.9.2") (dash "1.4.0") (request "0.1.0") (cl-lib "0.3.0") (json "1.3")))
