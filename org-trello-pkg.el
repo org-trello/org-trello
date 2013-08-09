d985dd396a139bd50d16a8e69921c728095e4491
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-08-09 23:31:48 +0200

    ** DONE Update version CLOSED: [2013-08-09 ven. 23:31]

--- org-trello-pkg.el
+++ org-trello-pkg.el
@@ -1,3 +1,3 @@
-(define-package "org-trello" "0.1.0"
-  "Minor mode for org-mode to sync org-mode and trello"
+(define-package "org-trello" "0.1.1"
+  "Org minor mode to synchronize with trello"
   '((org "8.0.7") (dash "1.4.0") (request "0.1.0") (cl-lib "0.3.0") (json "1.2")))
