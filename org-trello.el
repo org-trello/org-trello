82314c3b39c473b672cdba3e8e0e1db7689389bc
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-08-14 17:07:22 +0200

    Remove useless remark

--- org-trello.el
+++ org-trello.el
@@ -41,8 +41,6 @@
 ;; 3) Once per org-mode file/board you want to connect to (C-c o I)
 ;; M-x org-trello/install-board-and-lists-ids
 ;;
-;; *Beware* you must setup your trello board with the name you use as keywords (TODO, DONE e.g) on your org-mode file.
-;;
 ;; 4) You can also create a board directly from a org-mode buffer (C-c o b)
 ;; M-x org-trello/create-board
 ;;
