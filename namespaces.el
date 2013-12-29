6af2d0e472fe44ab8a54849bc6fae57947be4637
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-12-29 11:57:25 +0100

    The list is not to be reversed for all uses.

--- namespaces.el
+++ namespaces.el
@@ -1,16 +1,18 @@
 ;; Just to reference the org-trello `namespaces`
 
-(defvar *ORG-TRELLO-FILES* (nreverse '("src/header.el"
-                                       "src/setup.el"
-                                       "src/log.el"
-                                       "src/hash.el"
-                                       "src/cbx.el"
-                                       "src/data.el"
-                                       "src/api.el"
-                                       "src/query.el"
-                                       "src/action.el"
-                                       "src/proxy.el"
-                                       "src/webadmin.el"
-                                       "src/proxy-install.el"
-                                       "src/org-trello.el"
-                                       "src/footer.el")) "Splitted org-trello files.")
+(defvar *ORG-TRELLO-FILES* '("src/header.el"
+                             "src/setup.el"
+                             "src/log.el"
+                             "src/hash.el"
+                             "src/cbx.el"
+                             "src/data.el"
+                             "src/api.el"
+                             "src/query.el"
+                             "src/action.el"
+                             "src/proxy.el"
+                             "src/webadmin.el"
+                             "src/proxy-install.el"
+                             "src/org-trello.el"
+                             "src/footer.el") "Splitted org-trello files.")
+
+(provide 'org-trello-namespaces)
--- package.el
+++ package.el
@@ -2,9 +2,10 @@
 
 (defun org-trello/generate-one-file (one-file splitted-files) "From the `splitted-files` list, generate one `one-file` file."
   (with-temp-file one-file
-    (dolist (current-file splitted-files)
+    (dolist (current-file (nreverse splitted-files))
       (insert-file current-file))))
 
-(load-file "./namespaces.el") ;; this will load *ORG-TRELLO-FILES*
+(load-file "./namespaces.el")
+(require 'org-trello-namespaces)
 
 (org-trello/generate-one-file "org-trello.el" *ORG-TRELLO-FILES*)
