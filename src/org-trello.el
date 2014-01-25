cc6258f149edf82bad9c285934e9f91d96751640
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-25 18:06:06 +0100

    - [X] Use standard remapping to override the org-end-of-line routine - https://www.gnu.org/software/emacs/manual/html_node/elisp/Remapping-Commands.html

--- src/org-trello.el
+++ src/org-trello.el
@@ -206,7 +206,8 @@
 
 ;;;###autoload
 (define-minor-mode org-trello-mode "Sync your org-mode and your trello together."
-  :lighter    " ot")
+  :lighter " ot"
+  :keymap  (make-sparse-keymap))
 
 (defun org-trello-mode-on-hook-fn (&optional partial-mode) "Actions to do when org-trello starts."
   (unless partial-mode
@@ -221,7 +222,7 @@
           ;; a little message in the minibuffer to notify the user
           (orgtrello-log/msg *OT/NOLOG* (org-trello/--startup-message *ORGTRELLO-MODE-PREFIX-KEYBINDING*))
           ;; Overwrite the org-mode-map
-          (define-key org-mode-map "\C-e" 'org-trello/end-of-line!)))
+          (define-key org-trello-mode-map [remap org-end-of-line] 'org-trello/end-of-line!)))
 
 (defun org-trello-mode-off-hook-fn (&optional partial-mode) "Actions to do when org-trello stops."
   (unless partial-mode
@@ -233,8 +234,8 @@
           (remove-hook 'before-save-hook 'orgtrello-controller/install-overlays!)
           ;; remove org-trello overlays
           (orgtrello-controller/remove-overlays!)
-          ;; Reinstall the default org-mode-map
-          (define-key org-mode-map "\C-e" 'org-end-of-line)
+          ;; remove mapping override
+          (define-key org-trello-mode-map [remap org-end-of-line] nil)
           ;; a little message in the minibuffer to notify the user
           (orgtrello-log/msg *OT/NOLOG* "org-trello/ot is off!")))
 
