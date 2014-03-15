b014cbb48ce345895c98a5b9042984ece309254f
Antoine R. Dumont <eniotna.t@gmail.com>
2014-03-15 18:10:45 +0100

    Use mapc for loading namespaces

--- load-namespaces.el
+++ load-namespaces.el
@@ -1,14 +1,14 @@
 ;; Not designed to be used from shell - helper to load the splitted namespaces from src/ and still be able to browse source code from emacs
 
 (defun org-trello/load-ns (current-ns-file) "Load the current namespace file."
-  (message "org-trello file: %s loading..." current-ns-file)
+  (message "org-trello file: '%s' loading..." current-ns-file)
   (with-temp-file current-ns-file
     (insert-file-contents current-ns-file)
     (eval-buffer nil t current-ns-file t t)))
 
 (defun org-trello/load-namespaces (splitted-files) "Load the src files."
   (message "org-trello files: %s" splitted-files)
-  (mapcar 'org-trello/load-ns splitted-files))
+  (mapc 'org-trello/load-ns splitted-files))
 
 (load-file "./namespaces.el")
 (require 'org-trello-namespaces)
