75c0186a56513dd750fffb672c11c6a1aeace137
Antoine R. Dumont <eniotna.t@gmail.com>
2014-03-22 18:37:14 +0100

    - [X] Take control over the test file which is now too big (dispatch in 'namespace' test buffer)

---
+++ test/buffer-tests.el
@@ -0,0 +1,107 @@
+(require 'ert)
+(require 'ert-expectations)
+(require 'el-mock)
+
+(expectations
+  (expect "hello there\nhow are you today\nthis is a hell of a ride" (orgtrello-buffer/filter-out-properties ":PROPERTIES:
+:orgtrello-id: 52c945143004d4617c012528
+:END:
+hello there
+how are you today
+this is a hell of a ride"))
+  (expect "hello there\nhow are you today\nthis is a hell of a ride" (orgtrello-buffer/filter-out-properties "hello there
+how are you today
+this is a hell of a ride")))
+
+(expectations (desc "orgtrello-buffer/extract-description-from-current-position! - standard org-trello properties without blanks before them.")
+  (expect "hello there"
+    (orgtrello-tests/with-temp-buffer "* TODO Joy of FUN(ctional) LANGUAGES
+:PROPERTIES:
+:orgtrello-id: 52c945143004d4617c012528
+:END:
+hello there
+"
+      (orgtrello-buffer/extract-description-from-current-position!)))
+
+    (expect "hello there"
+      (orgtrello-tests/with-temp-buffer "* TODO Joy of FUN(ctional) LANGUAGES
+:PROPERTIES:
+:orgtrello-id: 52c945143004d4617c012528
+:END:
+hello there
+- [-] LISP family   :PROPERTIES: {\"orgtrello-id\":\"52c945140a364c5226007314\"}
+  - [X] Emacs-Lisp  :PROPERTIES: {\"orgtrello-id\":\"52c9451784251e1b260127f8\"}
+  - [X] Common-Lisp :PROPERTIES: {\"orgtrello-id\":\"52c94518b2c5b28e37012ba4\"}"
+       (orgtrello-buffer/extract-description-from-current-position!)))
+
+    (expect "hello there\n"
+      (orgtrello-tests/with-temp-buffer "* TODO Joy of FUN(ctional) LANGUAGES
+:PROPERTIES:
+:orgtrello-id: 52c945143004d4617c012528
+:END:
+
+hello there
+
+- [-] LISP family   :PROPERTIES: {\"orgtrello-id\":\"52c945140a364c5226007314\"}
+  - [X] Emacs-Lisp  :PROPERTIES: {\"orgtrello-id\":\"52c9451784251e1b260127f8\"}
+  - [X] Common-Lisp :PROPERTIES: {\"orgtrello-id\":\"52c94518b2c5b28e37012ba4\"}"
+       (orgtrello-buffer/extract-description-from-current-position!)))
+
+    (expect nil
+      (orgtrello-tests/with-temp-buffer "* TODO Joy of FUN(ctional) LANGUAGES" (orgtrello-buffer/extract-description-from-current-position!)))
+
+    (expect ""
+      (orgtrello-tests/with-temp-buffer "* TODO Joy of FUN(ctional) LANGUAGES
+:PROPERTIES:
+:orgtrello-id: 52c945143004d4617c012528
+:END:
+- [-] LISP family   :PROPERTIES: {\"orgtrello-id\":\"52c945140a364c5226007314\"}"
+       (orgtrello-buffer/extract-description-from-current-position!))))
+
+(expectations (desc "orgtrello-buffer/extract-description-from-current-position! - non standard org-trello properties with blanks before them.")
+  (expect "hello there"
+    (orgtrello-tests/with-temp-buffer "* TODO Joy of FUN(ctional) LANGUAGES
+ :PROPERTIES:
+ :orgtrello-id: 52c945143004d4617c012528
+ :END:
+hello there
+"
+      (orgtrello-buffer/extract-description-from-current-position!)))
+
+    (expect "hello there"
+      (orgtrello-tests/with-temp-buffer "* TODO Joy of FUN(ctional) LANGUAGES
+ :PROPERTIES:
+ :orgtrello-id: 52c945143004d4617c012528
+    :END:
+hello there
+- [-] LISP family   :PROPERTIES: {\"orgtrello-id\":\"52c945140a364c5226007314\"}
+  - [X] Emacs-Lisp  :PROPERTIES: {\"orgtrello-id\":\"52c9451784251e1b260127f8\"}
+  - [X] Common-Lisp :PROPERTIES: {\"orgtrello-id\":\"52c94518b2c5b28e37012ba4\"}"
+       (orgtrello-buffer/extract-description-from-current-position!)))
+
+    (expect "hello there\n"
+      (orgtrello-tests/with-temp-buffer "* TODO Joy of FUN(ctional) LANGUAGES
+  :PROPERTIES:
+         :orgtrello-id: 52c945143004d4617c012528
+  :END:
+
+hello there
+
+- [-] LISP family   :PROPERTIES: {\"orgtrello-id\":\"52c945140a364c5226007314\"}
+  - [X] Emacs-Lisp  :PROPERTIES: {\"orgtrello-id\":\"52c9451784251e1b260127f8\"}
+  - [X] Common-Lisp :PROPERTIES: {\"orgtrello-id\":\"52c94518b2c5b28e37012ba4\"}"
+       (orgtrello-buffer/extract-description-from-current-position!)))
+    (expect ""
+      (orgtrello-tests/with-temp-buffer "* TODO Joy of FUN(ctional) LANGUAGES
+  :PROPERTIES:
+ :orgtrello-id: 52c945143004d4617c012528
+:END:
+- [-] LISP family   :PROPERTIES: {\"orgtrello-id\":\"52c945140a364c5226007314\"}"
+                                        (orgtrello-buffer/extract-description-from-current-position!))))
+
+(expectations (desc "orgtrello-buffer/filter-out-properties - removing lines starting with org-trello metadata properties.")
+  (expect "no filter happens here." (orgtrello-buffer/filter-out-properties "no filter happens here."))
+  (expect "no filter happens here." (orgtrello-buffer/filter-out-properties "   no filter happens here."))
+  (expect "" (orgtrello-buffer/filter-out-properties ":PROPERTIES: filter happens and blank is left."))
+  (expect "" (orgtrello-buffer/filter-out-properties "  :PROPERTIES: filter still happens and blank is left."))
+  (expect "multiple lines\n" (orgtrello-buffer/filter-out-properties "  multiple lines\n  :PROPERTIES: filter still happens and blank is left.")))
---
+++ test/utils-tests.el
@@ -0,0 +1,44 @@
+(require 'ert)
+(require 'ert-expectations)
+(require 'el-mock)
+
+(defun hash-equal (hash1 hash2) "Compare two hash tables to see whether they are equal."
+  (and (= (hash-table-count hash1) (hash-table-count hash2))
+       (catch 'flag (maphash (lambda (x y) (or (equal (gethash x hash2) y) (throw 'flag nil))) hash1)
+              (throw 'flag t))))
+
+(expectations (desc "hash-equal")
+ (expect t (hash-equal (orgtrello-hash/make-properties `((:name . "some other name") (:keyword "TODO")))
+                       (orgtrello-hash/make-properties `((:name . "some other name") (:keyword "TODO")))))
+ (expect nil (hash-equal (orgtrello-hash/make-properties `((:name . "some other name") (:keyword "TODO")))
+                         (orgtrello-hash/make-properties `((:name . "some other name") (:keyword "DONE"))))))
+
+(expectations (desc "orgtrello-hash/make-transpose-properties")
+  (expect t (hash-equal (orgtrello-hash/make-properties `(("some other name" . :name) ("TODO" . :keyword)))
+                        (orgtrello-hash/make-transpose-properties `((:name . "some other name") (:keyword . "TODO"))))))
+
+(expectations (desc "orgtrello-hash/empty-hash")
+ (expect t (hash-equal #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8 data ())
+                       (orgtrello-hash/empty-hash))))
+
+(defun org-trello-mode-test () "Trigger org-trello-mode but shaped for the tests."
+  (remove-hook 'org-trello-mode-on-hook 'org-trello-mode-on-hook-fn)  (add-hook 'org-trello-mode-on-hook (lambda () (org-trello-mode-on-hook-fn t)))
+  (remove-hook 'org-trello-mode-off-hook 'org-trello-mode-off-hook-fn) (add-hook 'org-trello-mode-off-hook (lambda () (org-trello-mode-off-hook-fn t)) )
+  (org-trello-mode))
+
+(defmacro orgtrello-tests/with-temp-buffer (text body-test &optional nb-lines-forward)
+  `(with-temp-buffer
+     (org-mode)
+     (insert ,text)
+     (forward-line (if ,nb-lines-forward ,nb-lines-forward -1))
+     (org-trello-mode-test)
+     ,body-test))
+
+(defmacro orgtrello-tests/with-temp-buffer-and-return-buffer-content (text body-test &optional nb-line-forwards)
+  `(with-temp-buffer
+     (org-mode)
+     (insert ,text)
+     (forward-line (if ,nb-line-forwards ,nb-line-forwards -1))
+     (org-trello-mode-test)
+     ,body-test
+     (buffer-string)))
