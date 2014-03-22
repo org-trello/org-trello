7c1d812430b15623cd997e5c6704ee438b748a2e
Antoine R. Dumont <eniotna.t@gmail.com>
2014-03-22 23:53:15 +0100

    - [X] Version

--- org-trello-pkg.el
+++ org-trello-pkg.el
@@ -1,3 +1,3 @@
-(define-package "org-trello" "0.3.6"
+(define-package "org-trello" "0.3.6.1"
   "Minor mode to synchronize org-mode buffer and trello board"
   '((dash "2.5.0") (request "0.2.0") (elnode "0.9.9.7.6") (esxml "0.3.0") (s "1.7.0") (kv "0.0.19")))
