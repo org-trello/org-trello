b7071ee96bf4a6ab85a9283d79ccfb465bc4d449
Antoine R. Dumont <eniotna.t@gmail.com>
2014-03-23 21:49:14 +0100

    Use standard params

--- test/api-tests.el
+++ test/api-tests.el
@@ -14,13 +14,13 @@
 
 (expectations (desc "testing orgtrello-api/get-cards")
   (expect "GET"                                                                                                              (gethash :method (orgtrello-api/get-cards :board-id)))
-  (expect "/boards/:board-id/cards?actions=commentCard&field=closed,desc,due,idBoard,idChecklists,idList,idMembers,name,pos" (gethash :uri    (orgtrello-api/get-cards :board-id)))
-  (expect nil                                                                                                                (gethash :params (orgtrello-api/get-cards :board-id))))
+  (expect "/boards/:board-id/cards"                                                                                          (gethash :uri    (orgtrello-api/get-cards :board-id)))
+  (expect '(("actions" . "commentCard") ("field" . "closed,desc,due,idBoard,idChecklists,idList,idMembers,name,pos"))        (gethash :params (orgtrello-api/get-cards :board-id))))
 
 (expectations (desc "orgtrello-api/get-card")
-  (expect "GET"                                                                                                         (gethash :method (orgtrello-api/get-card :card-id)))
-  (expect "/cards/:card-id?actions=commentCard&actions_limit=5&action_fields=data&action_memberCreator_fields=username" (gethash :uri    (orgtrello-api/get-card :card-id)))
-  (expect nil                                                                                                           (gethash :params (orgtrello-api/get-card :card-id))))
+  (expect "GET"             (gethash :method (orgtrello-api/get-card :card-id)))
+  (expect "/cards/:card-id" (gethash :uri    (orgtrello-api/get-card :card-id)))
+  (expect '(("action_fields" "data") ("action_memberCreator_fields" . "username")) (gethash :params (orgtrello-api/get-card :card-id))))
 
 (expectations (desc "orgtrello-api/delete-card")
   (expect "DELETE"           (gethash :method (orgtrello-api/delete-card :card-id)))
