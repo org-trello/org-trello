cfc5df71a54666ce4470584975cff873f805c1a1
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-26 17:11:06 +0100

    Refactor - rename with convention regarding side effect

--- src/buffer.el
+++ src/buffer.el
@@ -1,21 +1,21 @@
 (defun orgtrello-buffer/back-to-card! () "Given the current position, goes on the card's heading"
   (org-back-to-heading))
 
-(defun orgtrello-buffer/--card-data-start-point () "Compute the first character of the card's text content."
+(defun orgtrello-buffer/--card-data-start-point! () "Compute the first character of the card's text content."
   (save-excursion
     (orgtrello-buffer/back-to-card!)
     (end-of-line)
     (1+ (point))))
 
-(defun orgtrello-buffer/--first-checkbox-point () "Compute the first position of the card's next checkbox."
+(defun orgtrello-buffer/--first-checkbox-point! () "Compute the first position of the card's next checkbox."
   (save-excursion
     (orgtrello-buffer/back-to-card!)
     (orgtrello-cbx/--goto-next-checkbox)
     (1- (point))))
 
 (defun orgtrello-buffer/extract-description-from-current-position! () "Given the current position, extract the text content of current card."
-  (let ((start (orgtrello-buffer/--card-data-start-point))
-        (end   (orgtrello-buffer/--first-checkbox-point)))
+  (let ((start (orgtrello-buffer/--card-data-start-point!))
+        (end   (orgtrello-buffer/--first-checkbox-point!)))
     (when (< start end)
           (orgtrello-buffer/filter-out-properties
            (buffer-substring-no-properties start end)))))
