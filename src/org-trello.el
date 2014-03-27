bbbd8ec50cae75357ddd4c9494cf3f99b3a0ae8a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-27 10:27:01 +0100

    Remove useless flag

--- src/org-trello.el
+++ src/org-trello.el
@@ -51,7 +51,7 @@
   "Control first, then if ok, sync a simple entity (without its structure)."
   (interactive "P")
   (if modifier
-      (org-trello/proxy-do "Request 'sync entity from trello'" 'orgtrello-controller/do-sync-entity-from-trello! *org-trello/with-save-flag*)
+      (org-trello/proxy-do "Request 'sync entity from trello'" 'orgtrello-controller/do-sync-entity-from-trello!)
     (org-trello/proxy-do "Request 'sync entity to trello'" 'orgtrello-controller/do-sync-entity-to-trello!)))
 
 (defun org-trello/sync-full-entity (&optional modifier)
