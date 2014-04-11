e0a37fd8dcd0df143dd52e5f4a3a44493d149545
Antoine R. Dumont <eniotna.t@gmail.com>
2014-04-11 22:27:09 +0200

    Refactor - During the loading time, add the dev namespace to load some dev tools

--- load-namespaces.el
+++ load-namespaces.el
@@ -1,6 +1,12 @@
 ;; Not designed to be used from shell - helper to load the splitted namespaces from src/ and still be able to browse source code from emacs
 
-(defvar *org-trello-namespaces-files* (expand-file-name "./namespaces.el") "Expand file so that we could use it directly with interactive command to reload code.")
+(defvar *org-trello-dir* (expand-file-name "."))
+
+(defun org-trello/full-file (filename)
+  "Compute the full file path"
+  (format "%s/%s" *org-trello-dir* filename))
+
+(defvar *org-trello-namespaces-files* (org-trello/full-file "namespaces.el") "Expand file so that we could use it directly with interactive command to reload code.")
 
 (defun org-trello/load-ns (current-ns-file) "Load the current namespace file."
   (message "org-trello file: '%s' loading..." current-ns-file)
@@ -16,7 +22,7 @@
 (defun org-trello/dev-load-namespaces ()
   "Load the namespace with interactive command"
   (interactive)
-  (load-file *org-trello-namespaces-files*)
+  (mapc 'load-file `(,*org-trello-namespaces-files* ,(org-trello/full-file "src/dev.el")))
   (org-trello/load-namespaces *ORG-TRELLO-FILES*))
 
 (org-trello/dev-load-namespaces)
