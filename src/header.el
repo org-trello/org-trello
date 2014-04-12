358a79e3c2b1e4f36ad5af3b47606893e250c500
Antoine R. Dumont <eniotna.t@gmail.com>
2014-04-12 08:50:47 +0200

    - [X] Version

--- src/header.el
+++ src/header.el
@@ -4,7 +4,7 @@
 
 ;; Author: Antoine R. Dumont <eniotna.t AT gmail.com>
 ;; Maintainer: Antoine R. Dumont <eniotna.t AT gmail.com>
-;; Version: 0.4.1.1
+;; Version: 0.4.2
 ;; Package-Requires: ((dash "2.5.0") (request "0.2.0") (elnode "0.9.9.7.6") (esxml "0.3.0") (s "1.7.0"))
 ;; Keywords: org-mode trello sync org-trello
 ;; URL: https://github.com/org-trello/org-trello
@@ -105,6 +105,6 @@ Please consider upgrading Emacs." emacs-version) "Error message when installing 
     (defalias 'cl-defun 'defun*)
     (defalias 'cl-destructuring-bind 'destructuring-bind)))
 
-(defconst *ORGTRELLO/VERSION* "0.4.1.1" "current org-trello version installed.")
+(defconst *ORGTRELLO/VERSION* "0.4.2" "current org-trello version installed.")
 
 
