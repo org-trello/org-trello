f34b37ad02976893c83d9d47266d4089f336eab7
Antoine R. Dumont <eniotna.t@gmail.com>
2014-05-10 23:58:22 +0200

    Doc - Clean doc convention

--- org-trello-hash.el
+++ org-trello-hash.el
@@ -1,35 +1,40 @@
-;;; org-trello-hash.el --- Hash manipulation functions (base data structure used by org-trello)
+;;; org-trello-hash.el --- Hash manipulation functions (the base data structure)
 ;;; Commentary:
 ;;; Code:
 
 (require 'org-trello-log)
 
 (defun orgtrello-hash/empty-hash ()
-  "Empty hash table with test 'equal"
+  "Empty hash table with test 'equal."
   (make-hash-table :test 'equal))
 
-(defun orgtrello-hash/gethash-data (key map &optional default-value) "Retrieve the map from some query-map" (when map (gethash key map default-value)))
+(defun orgtrello-hash/gethash-data (key map &optional default-value)
+  "Retrieve the value at KEY in MAP.
+If the value is not found, return DEFAULT-VALUE."
+  (when map (gethash key map default-value)))
 
 (defun orgtrello-hash/puthash-data (key value entity)
-  "Update the map at key with value. Return the entity updated. Nil if the entity is nil."
+  "Update at KEY the VALUE in the ENTITY map.
+Return the entity updated or nil if the entity is nil."
   (when entity
     (puthash key value entity)
     entity))
 
 (defun orgtrello-hash/make-properties (properties)
-  "Given a list of key value pair, return a hash table."
+  "Return a hash-table from PROPERTIES key/values."
   (--reduce-from (orgtrello-hash/puthash-data (car it) (cdr it) acc)
                  (orgtrello-hash/empty-hash)
                  properties))
 
 (defun orgtrello-hash/make-transpose-properties (properties)
-  "Given a list of key value pair, return a hash table with key/value transposed."
+  "Return a hash-table with transposed key/value from PROPERTIES key/values."
   (--reduce-from (orgtrello-hash/puthash-data (cdr it) (car it) acc)
                  (orgtrello-hash/empty-hash)
                  properties))
 
 (defun orgtrello-hash/init-map-from (data)
-  "Init a map from a given data. If data is nil, return an empty hash table."
+  "Init a map from a given DATA.
+If data is nil, return an empty hash table."
   (if data data (orgtrello-hash/empty-hash)))
 
 (orgtrello-log/msg *OT/DEBUG* "org-trello - orgtrello-hash loaded!")
