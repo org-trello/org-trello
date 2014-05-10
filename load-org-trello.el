589d22881465d9832946803b6d620d223eefc4d5
Antoine R. Dumont <eniotna.t@gmail.com>
2014-05-10 23:47:37 +0200

    Refactor - Force the means to load the code from scratch.

--- load-org-trello.el
+++ load-org-trello.el
@@ -2,6 +2,26 @@
 
 (add-to-list 'load-path (expand-file-name "."))
 
+(mapc (lambda (f) (load-file f)) '("org-trello-action.el"
+                                   "org-trello-api.el"
+                                   "org-trello-backend.el"
+                                   "org-trello-buffer.el"
+                                   "org-trello-cbx.el"
+                                   "org-trello-controller.el"
+                                   "org-trello-data.el"
+                                   "org-trello-db.el"
+                                   "org-trello-elnode.el"
+                                   "org-trello-hash.el"
+                                   "org-trello-input.el"
+                                   "org-trello-log.el"
+                                   "org-trello-proxy.el"
+                                   "org-trello-query.el"
+                                   "org-trello-server.el"
+                                   "org-trello-setup.el"
+                                   "org-trello-utils.el"
+                                   "org-trello-webadmin.el"
+                                   "org-trello.el"))
+
 (require 'org-trello)
 
 (provide 'load-org-trello)
