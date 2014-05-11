0e4b51d357d5ac195472569e0c411f9e0b8ad7e0
Antoine R. Dumont <eniotna.t@gmail.com>
2014-05-11 10:30:13 +0200

    Doc - clean docstring according to standard conventions

--- org-trello-api.el
+++ org-trello-api.el
@@ -9,44 +9,47 @@
 (require 'org-trello-data)
 
 (defun orgtrello-api/make-query (method uri &optional params)
-  "Utility function to ease the creation of the map - wait, where are my clojure data again!?"
+  "Make a query hash map from a METHOD, URI and optional parameter PARAMS."
   (let ((h (orgtrello-hash/empty-hash)))
     (->> (if params (orgtrello-data/put-entity-params params h) h)
       (orgtrello-data/put-entity-method method)
       (orgtrello-data/put-entity-uri uri))))
 
 (defun orgtrello-api/--deal-with-optional-value (optional-entry value entries)
-  "Add the optional value depending on the entry. Return entries updated with value if entry, entries untouched otherwise."
+  "Add the optional value in entries depending on optional-entry.
+If OPTIONAL-ENTRY is non nil, cons the VALUE to ENTRIES and return it.
+Otherwise,return ENTRIES."
   (if optional-entry (cons value entries) entries))
 
 (defun orgtrello-api/--deal-with-optional-values (optional-entries-values entries)
-  "Add the optional entry/value depending on their entry. Return entries updated with value if entry, entries untouched otherwise."
+  "Add the optional entry/value OPTIONAL-ENTRIES-VALUES in ENTRIES.
+Return entries updated with value if entry, entries untouched otherwise."
   (--reduce-from (orgtrello-api/--deal-with-optional-value (car it) (cdr it) acc)
                  entries
                  optional-entries-values))
 
 (defun orgtrello-api/add-board (name &optional description)
-  "Create a board."
+  "Create a board query from NAME and optional DESCRIPTION."
   (orgtrello-api/make-query "POST" "/boards" (orgtrello-api/--deal-with-optional-value description `("desc" . ,description) `(("name" . ,name)))))
 
 (defun orgtrello-api/get-boards ()
-  "Retrieve the boards of the current user."
+  "Create a retrieve boards of the current user query."
   (orgtrello-api/make-query "GET" "/members/me/boards"))
 
 (defun orgtrello-api/get-board (id)
-  "Retrieve the boards of the current user."
+  "Create a retrieve board with board ID query."
   (orgtrello-api/make-query "GET" (format "/boards/%s" id) '(("memberships" . "active")
                                                              ("memberships_member" . "true")
                                                              ("fields" . "name,memberships,closed,labelNames"))))
 
 (defun orgtrello-api/get-cards (board-id)
-  "cards of a board"
+  "Create a cards retrieval from the board with BOARD-ID query."
   (orgtrello-api/make-query "GET" (format "/boards/%s/cards" board-id)
                             '(("actions" .  "commentCard")
                               ("fields" . "closed,desc,due,idBoard,idChecklists,idList,idMembers,name,pos"))))
 
 (defun orgtrello-api/get-card (card-id)
-  "Detail of a card with id card-id."
+  "Create a get-card with CARD-ID query."
   (orgtrello-api/make-query "GET" (format "/cards/%s" card-id)
                             '(("actions" . "commentCard")
                               ("action_fields" . "data")
@@ -54,23 +57,23 @@
                               ("fields" .  "closed,dateLastActivity,desc,due,idChecklists,idList,idMembers,labels,name,pos"))))
 
 (defun orgtrello-api/delete-card (card-id)
-  "Delete a card with id card-id."
+  "Create a delete card with id CARD-ID query."
   (orgtrello-api/make-query "DELETE" (format "/cards/%s" card-id)))
 
 (defun orgtrello-api/get-lists (board-id)
-  "Display the lists of the board"
+  "Create a get-lists of the board with BOARD-ID."
   (orgtrello-api/make-query "GET" (format "/boards/%s/lists" board-id)))
 
 (defun orgtrello-api/close-list (list-id)
-  "'Close' the list with id list-id."
+  "Create a close list with id LIST-ID query."
   (orgtrello-api/make-query "PUT" (format "/lists/%s/closed" list-id) '((value . t))))
 
 (defun orgtrello-api/add-list (name idBoard)
-  "Add a list - the name and the board id are mandatory (so i say!)."
+  "Create an add a list with NAME and the IDBOARD."
   (orgtrello-api/make-query "POST" "/lists/" `(("name" . ,name) ("idBoard" . ,idBoard))))
 
 (defun orgtrello-api/add-card (name idList &optional due id-members desc labels)
-  "Add a card to a board, optional due date (formatted string date), id-members (csv id members) and description desc."
+  "Create an add a card with NAME to the list IDLIST, with optional DUE, ID-MEMBERS, DESC and LABELS query."
   (orgtrello-api/make-query "POST" "/cards/"
                             (orgtrello-api/--deal-with-optional-values `((,id-members . ("idMembers" . ,id-members))
                                                                          (,due . ("due" . ,due))
@@ -80,7 +83,7 @@
                                                                          ("idList" . ,idList)))))
 
 (defun orgtrello-api/move-card (card-id idList &optional name due id-members desc labels)
-  "Move a card to another list - optional entries (name, due date, id-members, desc)"
+  "Create an update a card CARD-ID to IDLIST with optional NAME, DUE date, ID-MEMBERS, DESC and LABELS query."
   (->> (orgtrello-api/--deal-with-optional-values `((,name . ("name" . ,name))
                                                     (,id-members . ("idMembers" . ,id-members))
                                                     (,due . ("due" . ,due))
@@ -90,15 +93,15 @@
     (orgtrello-api/make-query "PUT" (format "/cards/%s" card-id))))
 
 (defun orgtrello-api/add-checklist (card-id name)
-  "Add a checklist to a card"
+  "Create an add a checklist to a card CARD-ID, checklist with NAME query."
   (orgtrello-api/make-query "POST" (format "/cards/%s/checklists" card-id) `(("name" . ,name))))
 
 (defun orgtrello-api/update-checklist (checklist-id name)
-  "Update the checklist's name"
+  "Create an update the checklist CHECKLIST-ID with NAME query."
   (orgtrello-api/make-query "PUT" (format "/checklists/%s" checklist-id) `(("name" . ,name))))
 
 (defun orgtrello-api/get-checklist (checklist-id &optional without-items)
-  "Retrieve all the information from a checklist"
+  "Create a retrieve a checklist CHECKLIST-ID with optional WITHOUT-ITEMS flag query."
   (let ((default-params '(("fields" . "name,pos,idCard") ;; the id card is useful for us to determine if we deal with a checklist
                           ("checkItem_fields" . "name,pos,state"))))
     (orgtrello-api/make-query "GET"
@@ -106,33 +109,33 @@
                               (if without-items (cons '("checkItems" . "none") default-params) default-params))))
 
 (defun orgtrello-api/delete-checklist (checklist-id)
-  "Delete a checklist with checklist-id"
+  "Create a delete a checklist CHECKLIST-ID."
   (orgtrello-api/make-query "DELETE" (format "/checklists/%s" checklist-id)))
 
 (defun orgtrello-api/add-items (checklist-id name &optional checked)
-  "Add todo items (trello items) to a checklist with id 'id'"
+  "Create an add items to a checklist CHECKLIST-ID with NAME and optional CHECKED state query."
   (->> (orgtrello-api/--deal-with-optional-value checked `("checked" . ,checked) `(("name" . ,name)))
     (orgtrello-api/make-query "POST" (format "/checklists/%s/checkItems" checklist-id) )))
 
 (defun orgtrello-api/update-item (card-id checklist-id item-id name &optional state)
-  "Update an item"
+  "Create an update an item from the CARD-ID, CHECKLIST-ID and ITEM-ID with NAME and optional STATE query."
   (->> (orgtrello-api/--deal-with-optional-value state `("state" . ,state) `(("name" . ,name)))
     (orgtrello-api/make-query "PUT" (format "/cards/%s/checklist/%s/checkItem/%s" card-id checklist-id item-id))))
 
 (defun orgtrello-api/get-item (checklist-id item-id)
-  "List the checklist items."
+  "Create a get item from the checklist CHECKLIST-ID and item ITEM-ID query."
   (orgtrello-api/make-query "GET" (format "/checklists/%s/checkItems/%s" checklist-id item-id) '(("fields" . "name,pos,state"))))
 
 (defun orgtrello-api/delete-item (checklist-id item-id)
-  "Delete a item with id item-id"
+  "Create a delete from the checklist CHECKLIST-ID the item ITEM-ID query."
   (orgtrello-api/make-query "DELETE" (format "/checklists/%s/checkItems/%s" checklist-id item-id)))
 
 (defun orgtrello-api/get-me ()
-  "Retrieve the current user's member informations."
+  "Retrieve the current user's member informations query."
   (orgtrello-api/make-query "GET" "/members/me"))
 
 (defun orgtrello-api/add-card-comment (card-id comment-text)
-  "Add a comment to a card"
+  "Create a add a card comment to the card with id CARD-ID with the COMMENT-TEXT."
   (orgtrello-api/make-query "POST" (format "/cards/%s/actions/comments" card-id) `(("text" . ,comment-text))))
 
 (orgtrello-log/msg *OT/DEBUG* "org-trello - orgtrello-api loaded!")
--- org-trello-cbx.el
+++ org-trello-cbx.el
@@ -11,34 +11,34 @@
   (org-at-item-checkbox-p))
 
 (defun orgtrello-cbx/--to-properties (alist)
-  "Serialize an association list to json."
+  "Serialize an ALIST to json."
   (json-encode-hash-table (orgtrello-hash/make-properties alist)))
 
 (defun orgtrello-cbx/--from-properties (string)
-  "Deserialize from json to list."
+  "Deserialize STRING from json to list."
   (when string (json-read-from-string string)))
 
-(defun orgtrello-cbx/--checkbox-split (s)
-  "Split the checkbox into the checkbox data and the checkbox metadata."
-  (s-split ":PROPERTIES:" s))
+(defun orgtrello-cbx/--checkbox-split (checkbox-content)
+  "Split the checkbox CHECKBOX-CONTENT into the checkbox data and the checkbox metadata."
+  (s-split ":PROPERTIES:" checkbox-content))
 
-(defun orgtrello-cbx/--checkbox-metadata (s)
-  "Retrieve the checkbox's metadata."
-  (-when-let (res (-> s
+(defun orgtrello-cbx/--checkbox-metadata (checkbox-content)
+  "Retrieve the checkbox's metadata from the CHECKBOX-CONTENT."
+  (-when-let (res (-> checkbox-content
                     orgtrello-cbx/--checkbox-split
                     cadr))
     (s-trim-left res)))
 
-(defun orgtrello-cbx/--checkbox-data (s)
-  "Retrieve the checkbox's data."
-  (-> s
+(defun orgtrello-cbx/--checkbox-data (checkbox-content)
+  "Retrieve the checkbox's data from the CHECKBOX-CONTENT."
+  (-> checkbox-content
     orgtrello-cbx/--checkbox-split
     car
     s-trim-right))
 
-(defun orgtrello-cbx/--read-properties (s)
-  "Read the properties from the current string."
-  (->> s
+(defun orgtrello-cbx/--read-properties (string)
+  "Read the properties from the current STRING."
+  (->> string
     orgtrello-cbx/--checkbox-metadata
     orgtrello-cbx/--from-properties))
 
@@ -47,20 +47,22 @@
   (buffer-substring-no-properties (point-at-bol) (point-at-eol)))
 
 (defun orgtrello-cbx/--read-properties-from-point (pt)
-  "Read the properties from the current point."
+  "Read the properties from the point PT."
   (save-excursion
     (goto-char pt)
     (orgtrello-cbx/--read-properties (orgtrello-cbx/--read-checkbox!))))
 
 (defun orgtrello-cbx/--make-properties-as-string (properties)
+  "Serialize the PROPERTIES to checkbox string."
   (format ":PROPERTIES: %s" (orgtrello-cbx/--to-properties properties)))
 
 (defun orgtrello-cbx/remove-overlays! (start end)
-  "Remove the overlays presents between start and end in the current buffer"
+  "Remove the overlays presents between START and END in the current buffer"
   (remove-overlays start end 'invisible 'org-trello-cbx-property))
 
 (defun orgtrello-cbx/install-overlays! (start-position)
-  "Install org-trello overlays (first remove the current overlay on line)."
+  "Install org-trello overlays from START-POSITION.
+First, it removes the current overlay on line and then install the new."
   ;; remove overlay present on current position
   (orgtrello-cbx/remove-overlays! (point-at-bol) (point-at-eol))
   ;; build an overlay to hide the cbx properties
@@ -68,7 +70,7 @@
                'invisible 'org-trello-cbx-property))
 
 (defun orgtrello-cbx/--write-properties-at-point (pt properties)
-  "Given the new properties, update the current entry."
+  "Overwrite the current properties at point PT with new PROPERTIES."
   (save-excursion
     (goto-char pt)
     (let* ((checkbox-title   (-> (orgtrello-cbx/--read-checkbox!) orgtrello-cbx/--checkbox-data))
@@ -80,29 +82,30 @@
       text-to-insert)))
 
 (defun orgtrello-cbx/--key-to-search (key)
-  "Search the key key as a symbol"
+  "Search the KEY as a symbol."
   (if (stringp key) (intern key) key))
 
 (defun orgtrello-cbx/--org-get-property (key properties)
-  "Internal accessor to the key property."
+  "Retrieve the value at KEY in PROPERTIES."
   (-> key
     orgtrello-cbx/--key-to-search
     (assoc-default properties)))
 
 (defun orgtrello-cbx/--org-update-property (key value properties)
-  "Internal accessor to the key property."
+  "Update the KEY with VALUE in PROPERTIES."
   (->> properties
     (orgtrello-cbx/--org-delete-property key)
     (cons `(,(orgtrello-cbx/--key-to-search key) . ,value))))
 
 (defun orgtrello-cbx/--org-delete-property (key properties)
-  "Delete the key from the properties."
+  "Delete the KEY from the PROPERTIES."
   (-> key
     orgtrello-cbx/--key-to-search
     (assq-delete-all properties)))
 
 (defun orgtrello-cbx/org-set-property (key value)
-  "Read the properties. Add the new property key with the value value. Write the new properties."
+  "Add the new property KEY with VALUE.
+Write the new properties at current position."
   (let ((current-point (point)))
     (->> current-point
       orgtrello-cbx/--read-properties-from-point
@@ -110,38 +113,38 @@
       (orgtrello-cbx/--write-properties-at-point current-point))))
 
 (defun orgtrello-cbx/org-get-property (point key)
-  "Retrieve the value for the key key."
+  "Retrieve the value at POINT with property KEY."
   (->> point
     orgtrello-cbx/--read-properties-from-point
     (orgtrello-cbx/--org-get-property key)))
 
 (defun orgtrello-cbx/org-delete-property (key)
-  "Delete the property key from the properties."
+  "Delete the property KEY from the properties."
   (let ((current-point (point)))
     (->> current-point
       orgtrello-cbx/--read-properties-from-point
       (orgtrello-cbx/--org-delete-property key)
       (orgtrello-cbx/--write-properties-at-point current-point))))
 
-(defun orgtrello-cbx/--org-split-data (s)
-  "Split the string into meta data with -."
-  (->> s
+(defun orgtrello-cbx/--org-split-data (string)
+  "Split the STRING into meta data with -."
+  (->> string
     (s-replace "[ ]" "[]")
     (s-split " ")))
 
-(defun orgtrello-cbx/--retrieve-status (l)
-  "Given a list of metadata, return the status"
+(defun orgtrello-cbx/--retrieve-status (metadata)
+  "Given a list of METADATA, return the status."
   (car (--drop-while (not (or (string= "[]" it)
                               (string= "[X]" it)
                               (string= "[-]" it)
-                              (string= "[ ]" it))) l)))
+                              (string= "[ ]" it))) metadata)))
 
-(defun orgtrello-cbx/--status (s)
-  "Given a checklist status, return the TODO/DONE for org-trello to work."
-  (if (string= "[X]" s) *ORGTRELLO/DONE* *ORGTRELLO/TODO*))
+(defun orgtrello-cbx/--status (status)
+  "Given a checklist STATUS, return the TODO/DONE for org-trello to work."
+  (if (string= "[X]" status) *ORGTRELLO/DONE* *ORGTRELLO/TODO*))
 
 (defun orgtrello-cbx/--name (s status)
-  "Retrieve the name of the checklist"
+  "Retrieve the name of the checklist from the checkbox content S and its STATUS."
   (->> s
     (s-replace "[ ]" "[]")
     s-trim-left
@@ -151,7 +154,7 @@
     s-trim))
 
 (defun orgtrello-cbx/--metadata-from-checklist (full-checklist)
-  "Given a checklist string, extract the list of metadata"
+  "Given a FULL-CHECKLIST string, extract the list of metadata."
   (let* ((checklist-data   (orgtrello-cbx/--checkbox-data full-checklist))
          (meta             (orgtrello-cbx/--org-split-data checklist-data))
          (status-retrieved (orgtrello-cbx/--retrieve-status meta)))
@@ -177,11 +180,12 @@ This is a list with the following elements:
           (orgtrello-cbx/--metadata-from-checklist (orgtrello-cbx/--read-checkbox!)))))
 
 (defun orgtrello-cbx/--get-level (meta)
-  "Retrieve the level from the meta describing the checklist"
+  "Retrieve the level from the META describing the checklist."
   (car meta))
 
 (defun orgtrello-cbx/--org-up! (destination-level)
-  "An internal function to get back to the current entry's parent - return the level found or nil if the level found is a card."
+  "A function to get back to the current entry's DESTINATION-LEVEL ancestor.
+Return the level found or nil if the level found is a card."
   (let ((current-level (orgtrello-cbx/--get-level (orgtrello-cbx/org-checkbox-metadata!))))
     (cond ((= *ORGTRELLO/CARD-LEVEL*      current-level) nil)
           ((= destination-level current-level) destination-level)
@@ -201,19 +205,26 @@ This is a list with the following elements:
     orgtrello-cbx/--org-up!))
 
 (defun orgtrello-cbx/compute-next-card-point! ()
-  "Compute the next card's position. Does preserve position. If a sibling is found, return the point-at-bol, otherwise return the max point in buffer."
+  "Compute the next card's position.
+Does preserve position.
+If a sibling is found, return the point-at-bol, otherwise return the max point in buffer."
   (save-excursion
     (org-back-to-heading)
     (if (org-goto-sibling) (point-at-bol) (point-max))))
 
 (defun orgtrello-cbx/--goto-next-checkbox ()
-  "Compute the next checkbox's beginning of line. Does not preserve the current position. If hitting a heading or the end of the file, return nil."
+  "Compute the next checkbox's beginning of line.
+Does not preserve the current position.
+If hitting a heading or the end of the file, return nil."
   (forward-line)
   (when (and (not (org-at-heading-p)) (< (point) (point-max)) (not (orgtrello-cbx/checkbox-p)))
     (orgtrello-cbx/--goto-next-checkbox)))
 
 (defun orgtrello-cbx/--goto-next-checkbox-with-same-level! (level)
-  "Compute the next checkbox's beginning of line (with the same level). Does not preserve the current position. If hitting a heading or the end of the file, return nil. Otherwise, return the current position."
+  "Compute the next checkbox's beginning of line (with the same LEVEL).
+ Does not preserve the current position.
+If hitting a heading or the end of the file, return nil.
+Otherwise, return the current position."
   (forward-line)
   (if (= level (orgtrello-cbx/current-level!))
       (point)
@@ -222,20 +233,22 @@ This is a list with the following elements:
       (orgtrello-cbx/--goto-next-checkbox-with-same-level! level))))
 
 (defun orgtrello-cbx/--map-checkboxes (level fn-to-execute)
-  "Map over the checkboxes and execute fn when in checkbox. Does not preserve the cursor position. Do not exceed the point-max."
+  "Map over the checkboxes with level > to LEVEL and execute FN-TO-EXECUTE.
+Does not preserve the cursor position.
+Do not exceed the max size of buffer."
   (orgtrello-cbx/--goto-next-checkbox)
   (when (< level (orgtrello-cbx/current-level!))
     (funcall fn-to-execute)
     (orgtrello-cbx/--map-checkboxes level fn-to-execute)))
 
 (defun orgtrello-cbx/map-checkboxes (fn-to-execute)
-  "Map over the current checkbox and sync them."
+  "Map over the current checkbox and execute FN-TO-EXECUTE."
   (let ((level (orgtrello-cbx/current-level!)))
     (when (= level *ORGTRELLO/CHECKLIST-LEVEL*) (funcall fn-to-execute))
     (save-excursion (orgtrello-cbx/--map-checkboxes level fn-to-execute)))) ;; then map over the next checkboxes and sync them
 
 (defun orgtrello-cbx/next-checklist-point! ()
-  "Compute the next checklist position"
+  "Compute the next checklist position."
   (-if-let (next-checklist-point (save-excursion (orgtrello-cbx/--goto-next-checkbox-with-same-level! *ORGTRELLO/CHECKLIST-LEVEL*) (point)))
       next-checklist-point
     (orgtrello-cbx/compute-next-card-point!)))
