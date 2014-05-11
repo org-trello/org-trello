22b8fa1f1d305ba770b296334a1d9503fe6908dd
Antoine R. Dumont <eniotna.t@gmail.com>
2014-05-11 13:28:40 +0200

    Define a function and a binding to help in loading org-trello namespaces.

--- load-org-trello.el
+++ load-org-trello.el
@@ -5,25 +5,32 @@
 
 (add-to-list 'load-path (expand-file-name "."))
 
-(mapc #'load-file '("org-trello-action.el"
-                    "org-trello-api.el"
-                    "org-trello-backend.el"
-                    "org-trello-buffer.el"
-                    "org-trello-cbx.el"
-                    "org-trello-controller.el"
-                    "org-trello-data.el"
-                    "org-trello-db.el"
-                    "org-trello-elnode.el"
-                    "org-trello-hash.el"
-                    "org-trello-input.el"
-                    "org-trello-log.el"
-                    "org-trello-proxy.el"
-                    "org-trello-query.el"
-                    "org-trello-server.el"
-                    "org-trello-setup.el"
-                    "org-trello-utils.el"
-                    "org-trello-webadmin.el"
-                    "org-trello.el"))
+(defun org-trello/dev-load-namespaces! ()
+  "Load the org-trello namespaces."
+  (interactive)
+  (mapc #'load-file '("org-trello-action.el"
+                      "org-trello-api.el"
+                      "org-trello-backend.el"
+                      "org-trello-buffer.el"
+                      "org-trello-cbx.el"
+                      "org-trello-controller.el"
+                      "org-trello-data.el"
+                      "org-trello-db.el"
+                      "org-trello-elnode.el"
+                      "org-trello-hash.el"
+                      "org-trello-input.el"
+                      "org-trello-log.el"
+                      "org-trello-proxy.el"
+                      "org-trello-query.el"
+                      "org-trello-server.el"
+                      "org-trello-setup.el"
+                      "org-trello-utils.el"
+                      "org-trello-webadmin.el"
+                      "org-trello.el")))
+
+(global-set-key (kbd "C-c o l") 'org-trello/dev-load-namespaces!)
+
+(org-trello/dev-load-namespaces!)
 
 (require 'org-trello)
 
