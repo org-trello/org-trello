43fc3c43b5b2b8e8ba48cce81b58c2c4f5e32bdb
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-08-20 23:31:51 +0200

    - [X] Remove db dependency

--- org-trello.el
+++ org-trello.el
@@ -5,7 +5,7 @@
 ;; Author: Antoine R. Dumont <eniotna.t AT gmail.com>
 ;; Maintainer: Antoine R. Dumont <eniotna.t AT gmail.com>
 ;; Version: 0.5.1
-;; Package-Requires: ((dash "2.7.0") (request "0.2.0") (elnode "0.9.9.8.1") (esxml "0.3.0") (s "1.9.0") (db "0.0.6"))
+;; Package-Requires: ((dash "2.7.0") (request "0.2.0") (elnode "0.9.9.8.1") (esxml "0.3.0") (s "1.9.0"))
 ;; Keywords: org-mode trello sync org-trello
 ;; URL: https://github.com/org-trello/org-trello
 
@@ -96,7 +96,6 @@ Please consider upgrading Emacs." emacs-version) "Error message when installing 
 (require 'elnode)
 (require 's)
 (require 'esxml)
-(require 'db)
 
 (defconst *ORGTRELLO/VERSION* "0.5.1" "Current org-trello version installed.")
 
