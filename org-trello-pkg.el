e790adefccac4312f1430c29f07c3138a4ac2507
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-08-21 01:20:29 +0200

    - [X] Update org-trello package

--- org-trello-pkg.el
+++ org-trello-pkg.el
@@ -1,7 +1,5 @@
 (define-package "org-trello" "0.5.1" "Minor mode to synchronize org-mode buffer and trello board"
-  '((db "0.0.6")
+  '((deferred "0.3.2")
     (s "1.9.0")
-    (esxml "0.3.0")
-    (elnode "0.9.9.8.1")
     (request "0.2.0")
     (dash "2.7.0")))
