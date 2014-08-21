63f1bbaec0b062a44266735e25fece9e7ee893f9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-08-21 20:37:54 +0200

    Add function to ease finding dead code

--- load-org-trello.el
+++ load-org-trello.el
@@ -4,26 +4,42 @@
 
 (add-to-list 'load-path (expand-file-name "."))
 
+(defvar *ORGTRELLO-NAMESPACES* '("org-trello-action.el"
+                                 "org-trello-api.el"
+                                 "org-trello-backend.el"
+                                 "org-trello-buffer.el"
+                                 "org-trello-cbx.el"
+                                 "org-trello-controller.el"
+                                 "org-trello-data.el"
+                                 "org-trello-hash.el"
+                                 "org-trello-input.el"
+                                 "org-trello-log.el"
+                                 "org-trello-proxy.el"
+                                 "org-trello-query.el"
+                                 "org-trello-setup.el"
+                                 "org-trello-utils.el"
+                                 "org-trello.el")
+  "Org-trello namespaces for development purposes.")
+
 (defun org-trello/dev-load-namespaces! ()
   "Load the org-trello namespaces."
   (interactive)
-  (mapc (lambda (it) (load-with-code-conversion it it)) '("org-trello-action.el"
-                                                          "org-trello-api.el"
-                                                          "org-trello-backend.el"
-                                                          "org-trello-buffer.el"
-                                                          "org-trello-cbx.el"
-                                                          "org-trello-controller.el"
-                                                          "org-trello-data.el"
-                                                          "org-trello-hash.el"
-                                                          "org-trello-input.el"
-                                                          "org-trello-log.el"
-                                                          "org-trello-proxy.el"
-                                                          "org-trello-query.el"
-                                                          "org-trello-setup.el"
-                                                          "org-trello-utils.el"
-                                                          "org-trello.el")))
+  (mapc (lambda (it) (load-with-code-conversion it it)) *ORGTRELLO-NAMESPACES*))
+
+(defun org-trello/dev-find-unused-definitions! ()
+  "Find unused definitions."
+  (interactive)
+  (with-temp-file "/tmp/org-trello-find-unused-definitions.el"
+    (erase-buffer)
+    (mapc (lambda (it)
+            (insert-file-contents it)
+            (goto-char (point-max))) *ORGTRELLO-NAMESPACES*)
+    (write-file "/tmp/org-trello-find-unused-definitions.el")
+    (emacs-lisp-mode)
+    (call-interactively 'emr-el-find-unused-definitions)))
 
 (global-set-key (kbd "C-c o n") 'org-trello/dev-load-namespaces!)
+(global-set-key (kbd "C-c o f") 'org-trello/dev-find-unused-definitions!)
 
 (org-trello/dev-load-namespaces!)
 (message "org-trello loaded!")
