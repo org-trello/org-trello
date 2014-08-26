44abda216e452326ab7f83610796fa7f418f940e
Antoine R. Dumont <eniotna.t@gmail.com>
2014-08-26 19:47:18 +0200

    - [X] Update package doc installation in org-trello.el

--- org-trello.el
+++ org-trello.el
@@ -31,6 +31,9 @@
 ;; Minor mode to sync org-mode buffer and trello board
 ;;
 ;; 1) Add the following to your Emacs init file
+;; - Either activate org-trello-mode in an org-buffer - M-x org-trello-mode
+;;
+;; - Or add this in your Emacs setup
 ;; (require 'org-trello)
 ;; (add-hook 'org-mode-hook 'org-trello-mode)
 ;;
@@ -41,6 +44,9 @@
 ;; - to connect your org buffer to an existing board (C-c o I).  Beware that this will only install properties needed to speak with trello board (nothing else).
 ;; M-x org-trello/install-board-metadata
 ;;
+;; - to update an existing org-buffer connected to a trello board (C-c o u).
+;; M-x org-trello/update-board-metadata
+;;
 ;; - to create an empty board directly from a org-mode buffer (C-c o b)
 ;; M-x org-trello/create-board
 ;;
@@ -50,26 +56,39 @@
 ;; 6) For some more help (C-c o h)
 ;; M-x org-trello/help-describing-setup
 ;;
-;; 7) If you attached to an existing trello board, you may want to bootstrap your org-buffer (C-u C-c o s)
+;; 7) The first time you attached your buffer to an existing trello board, you may want to bootstrap your org-buffer (C-u C-c o s)
 ;; C-u M-x org-trello/sync-buffer
 ;;
-;; Now you can work with trello from the comfort of org-mode and Emacs
-;;
-;; 8) Sync a card from Org to Trello (C-c o c/C-c o C)
+;; 8) Sync a card from Org to Trello (C-c o c / C-c o C)
 ;; M-x org-trello/sync-card
 ;;
-;; 9) Sync a card from Trello to Org (C-u C-c o c/C-u C-c o C)
+;; 9) Sync a card from Trello to Org (C-u C-c o c / C-u C-c o C)
 ;; C-u M-x org-trello/sync-card
 ;;
-;; 10) Sync all the org buffer to trello (C-c o s)
+;; 10) Sync complete org buffer to trello (C-c o s)
 ;; M-x org-trello/sync-buffer
 ;;
 ;; 11) As already mentioned, you can sync all the org buffer from trello (C-u C-c o s)
 ;; C-u M-x org-trello/sync-buffer
 ;;
+;; 12) You can delete an entity, card/checklist/item at point (C-c o k)
+;; M-x org-trello/kill-entity
+;;
+;; 13) You can delete all the cards (C-c o K / C-u C-c o k)
+;; M-x org-trello/kill-cards / C-u M-x org-trello/kill-entity
+;;
+;; 14) You can directly jump to the trello card in the browser (C-c o j)
+;; M-x org-trello/jump-to-trello-card
+;;
+;; 15) You can directly jump to the trello board in the browser (C-c o J / C-u C-c o j)
+;; M-x org-trello/jump-to-trello-board / C-u M-x org-trello/jump-to-trello-card
+;;
+;; Now you can work with trello from the comfort of org-mode and Emacs
+;;
 ;; Enjoy!
 ;;
-;; More informations on https://org-trello.github.io/org-trello
+;; More informations: https://org-trello.github.io/org-trello
+;; Issue tracker: https://github.com/org-trello/org-trello/issues
 
 ;;; Code:
 
