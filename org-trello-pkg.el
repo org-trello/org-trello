1ebb3c4d6669f1698dda6f75258f7060ec78c04f
Antoine R. Dumont <eniotna.t@gmail.com>
2014-09-01 22:53:29 +0200

    - [X] Downgrade request dependency to 0.3.1 to make org-trello installation possible in melpa-stable (no answer from `request` maintainer) + https://github.com/org-trello/org-trello/issues/195

--- org-trello-pkg.el
+++ org-trello-pkg.el
@@ -1,5 +1,5 @@
 (define-package "org-trello" "0.5.8" "Minor mode to synchronize org-mode buffer and trello board"
-  '((request-deferred "0.2.0")
+  '((request-deferred "0.1.0")
     (deferred "0.3.2")
     (s "1.9.0")
     (dash "2.8.0")
