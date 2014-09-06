ab04b6619fa32ee84c50e2cfb90ade808f0ff9f4
Antoine R. Dumont <eniotna.t@gmail.com>
2014-09-06 09:06:48 +0200

    - [X] Use defcustom instead of defvar for user customisation - [[https://github.com/org-trello/org-trello/issues/196][#196]]

---
+++ test/org-trello-setup-tests.el
@@ -0,0 +1,34 @@
+(require 'org-trello-setup)
+(require 'ert)
+(require 'ert-expectations)
+(require 'el-mock)
+
+(expectations (desc "ORGTRELLO/MODE-PREFIX-KEYBINDING*")
+ (expect "C-c o a - M-x some-action - some-description
+C-c o 2 - M-x action2 - some other description" (orgtrello-setup/help-describing-bindings-template "C-c o" '((some-action "a" "some-description")
+                                                                                                          (action2 "2" "some other description")))))
+(expectations (desc "orgtrello-setup/help-describing-bindings-template")
+  (expect
+      "C-c o v - M-x org-trello/version - Display the current version installed.
+C-c o i - M-x org-trello/install-key-and-token - Install the keys and the access-token.
+C-c o I - M-x org-trello/install-board-metadata - Select the board and attach the todo, doing and done list.
+C-c o u - M-x org-trello/update-board-metadata - Update the buffer's trello board metadata.
+C-c o b - M-x org-trello/create-board-and-install-metadata - Create interactively a board and attach the newly created trello board with the current org file.
+C-c o d - M-x org-trello/check-setup - Check that the setup is ok. If everything is ok, will simply display 'Setup ok!'.
+C-c o D - M-x org-trello/delete-setup - Clean up the org buffer from all org-trello informations.
+C-c o c - M-x org-trello/sync-card - Create/Update a complete card.
+C-c o s - M-x org-trello/sync-buffer - Synchronize the org-mode file to the trello board (org-mode -> trello). With prefix C-u, sync-from-trello (org-mode <- trello).
+C-c o A - M-x org-trello/archive-cards - Archive all DONE cards.
+C-c o g - M-x org-trello/abort-sync - Abort synchronization activities.
+C-c o k - M-x org-trello/kill-entity - Kill the entity (and its arborescence tree) from the trello board and the org buffer.
+C-c o K - M-x org-trello/kill-cards - Kill all the entities (and their arborescence tree) from the trello board and the org buffer.
+C-c o a - M-x org-trello/assign-me - Assign oneself to the card. With C-u modifier, unassign oneself from the card.
+C-c o C - M-x org-trello/add-card-comments - Add a comment to the card. With C-u modifier, display the current card's comments in a pop-up buffer.
+C-c o l - M-x org-trello/show-board-labels - Display the board's labels in a pop-up buffer.
+C-c o j - M-x org-trello/jump-to-trello-card - Jump to card in browser.
+C-c o J - M-x org-trello/jump-to-trello-board - Open the browser to your current trello board.
+C-c o h - M-x org-trello/help-describing-bindings - This help message."
+      (orgtrello-setup/help-describing-bindings-template *ORGTRELLO/MODE-PREFIX-KEYBINDING* *org-trello-interactive-command-binding-couples*)))
+
+(expectations (desc "orgtrello-data/--merge-member-ids")
+              (expect "org-trello/ot is on! To begin with, hit C-c o h or M-x 'org-trello/help-describing-bindings" (orgtrello-setup/startup-message "C-c o")))
