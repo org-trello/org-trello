5656f32d3624b3c82014658aef88ffa47c0fca7b
Antoine R. Dumont <eniotna.t@gmail.com>
2015-03-15 23:50:29 +0100

    Update request-deferred to 0.2.0

--- org-trello-pkg.el
+++ org-trello-pkg.el
@@ -1,5 +1,5 @@
 (define-package "org-trello" "0.6.9.2" "Minor mode to synchronize org-mode buffer and trello board"
-  '((request-deferred "0.1.0")
+  '((request-deferred "0.2.0")
     (deferred "0.3.2")
     (s "1.9.0")
     (dash "2.8.0")
