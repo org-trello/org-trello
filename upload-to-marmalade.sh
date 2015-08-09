6fa184d1f5b8c6a83dfe8afa86e89ec82eb16668
Antoine R. Dumont <eniotna.t@gmail.com>
2015-08-10 00:04:06 +0200

    - [X] Improve release + upload to marmalade scripts

--- upload-to-marmalade.sh
+++ upload-to-marmalade.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 # depends on curl
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
     cat <<EOF
 Use: $0 <VERSION>"
 - VERSION    version to release (0.1.6 for example)
+- PACKAGE    package to release
 
 EOF
     exit 1;
 fi
 
 VERSION=$1
+PACKAGE=$2
 
 uploadFile () {
     local username=$1
@@ -24,4 +26,4 @@ uploadFile () {
         $marmaladeUrl/v1/packages
 }
 
-uploadFile ardumont ~/.marmalade/token ./org-trello-$VERSION.tar
+uploadFile ardumont ~/.marmalade/token ./$PACKAGE-$VERSION.tar
