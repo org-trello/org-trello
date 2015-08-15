edaf55054b0bad20e5ea5ec6f8caae01a735912d
Antoine R. Dumont <eniotna.t@gmail.com>
2015-08-15 21:11:24 +0200

    Add coverage on org-trello-log namespace + fix bug

--- org-trello-log.el
+++ org-trello-log.el
@@ -2,23 +2,24 @@
 ;;; Commentary:
 ;;; Code:
 
-(defconst orgtrello-log-no-log 0 "No log level.")
-(defconst orgtrello-log-error  1 "Error log level.")
-(defconst orgtrello-log-warn   2 "Warn log level.")
+(defconst orgtrello-log-no-log 0 "No log level except for error.")
+(defconst orgtrello-log-error  5 "Error log level.")
+(defconst orgtrello-log-warn   4 "Warn log level.")
 (defconst orgtrello-log-info   3 "Info log level.")
-(defconst orgtrello-log-debug  4 "Debug log level.")
-(defconst orgtrello-log-trace  5 "Trace log level.")
+(defconst orgtrello-log-debug  2 "Debug log level.")
+(defconst orgtrello-log-trace  1 "Trace log level.")
 
 (defcustom orgtrello-log-level orgtrello-log-info
   "Set log level.
 Levels:
-0 - no logging   (`'orgtrello-log-no-log`')
-1 - log errors   (`'orgtrello-log-error`')
-2 - log warnings (`'orgtrello-log-warn`')
+0 - no logging   (`'orgtrello-log-quiet`')
+5 - log errors   (`'orgtrello-log-error`')
+4 - log warnings (`'orgtrello-log-warn`')
 3 - log info     (`'orgtrello-log-info`')
-4 - log debug    (`'orgtrello-log-debug`')
-5 - log trace    (`'orgtrello-log-trace`')
-To change such level, add this to your init.el file: (setq orgtrello-log-level orgtrello-log-trace)"
+2 - log debug    (`'orgtrello-log-debug`')
+1 - log trace    (`'orgtrello-log-trace`')
+To change such level, add this to your init.el file:
+\(custom-set-variables '\(orgtrello-log-level orgtrello-log-trace\)\)"
   :options (list orgtrello-log-no-log orgtrello-log-error orgtrello-log-warn orgtrello-log-info orgtrello-log-debug orgtrello-log-trace)
   :type 'integer
   :require 'org-trello
@@ -26,8 +27,10 @@ To change such level, add this to your init.el file: (setq orgtrello-log-level o
 
 (defun orgtrello-log-msg (level &rest args)
   "Log message with LEVEL.
+Depending on `orgtrello-log-level', this will be displayed or not.
+All errors are displayed anyway.
 ARGS constitutes the parameters to feed to message."
-  (when (<= level orgtrello-log-level)
+  (when (or (<= level orgtrello-log-level) (eq orgtrello-log-error level))
     (apply 'message (format "org-trello - %s" (car args)) (cdr args))))
 
 (orgtrello-log-msg orgtrello-log-debug "orgtrello-log loaded!")
