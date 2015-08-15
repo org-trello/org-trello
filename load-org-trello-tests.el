281aa71c10789b5e2c5ba644b1c2dc53cf488c1b
Antoine R. Dumont <eniotna.t@gmail.com>
2015-08-16 00:09:55 +0100

    Refactor - Fix macro definition inside tests ns

--- load-org-trello-tests.el
+++ load-org-trello-tests.el
@@ -102,6 +102,13 @@ Otherwise, default to current buffer."
 (define-key emacs-lisp-mode-map (kbd "C-c o d") 'orgtrello-tests-load-namespaces)
 (define-key emacs-lisp-mode-map (kbd "C-c o D") 'orgtrello-tests-find-next-error)
 
+(fset 'orgtrello-tests-org-raw-coverage
+      (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([201326624 201326624 201326624 134217848 111 114 103 116 114 101 108 108 111 45 116 101 115 116 115 45 99 111 117 110 116 45 102 117 110 99 116 105 111 110 115 13 67108896 134217826 23 9 25 46 48 9 201326624 201326624 201326624 134217848 111 114 103 116 114 101 108 108 111 45 99 111 8 8 116 101 115 116 115 45 99 111 117 110 116 45 110 117 109 98 101 114 45 116 101 115 116 115 13 67108896 134217826 23 9 25 46 48 9 9] 0 "%d")) arg)))
+
+(define-key org-trello-mode-map (kbd "C-c o o") 'org-trello-dev-find-unused-definitions)
+(define-key emacs-lisp-mode-map (kbd "C-c o o") 'orgtrello-tests-org-raw-coverage)
+
+
 (require 'org-trello-action-test)
 (require 'org-trello-api-test)
 (require 'org-trello-backend-test)
--- load-org-trello.el
+++ load-org-trello.el
@@ -56,15 +56,10 @@
 
 (require 'org-trello)
 
-(fset 'orgtrello-tests-org-raw-coverage
-      (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([201326624 201326624 201326624 134217848 111 114 103 116 114 101 108 108 111 45 116 101 115 116 115 45 99 111 117 110 116 45 102 117 110 99 116 105 111 110 115 13 67108896 134217826 23 9 25 46 48 9 201326624 201326624 201326624 134217848 111 114 103 116 114 101 108 108 111 45 99 111 8 8 116 101 115 116 115 45 99 111 117 110 116 45 110 117 109 98 101 114 45 116 101 115 116 115 13 67108896 134217826 23 9 25 46 48 9 9] 0 "%d")) arg)))
-
 (define-key emacs-lisp-mode-map (kbd "C-c o r") 'org-trello-dev-load-namespaces)
 (define-key org-trello-mode-map (kbd "C-c o r") 'org-trello-dev-load-namespaces)
 (define-key emacs-lisp-mode-map (kbd "C-c o f") 'org-trello-dev-find-unused-definitions)
 (define-key org-trello-mode-map (kbd "C-c o f") 'org-trello-dev-find-unused-definitions)
-(define-key org-trello-mode-map (kbd "C-c o o") 'org-trello-dev-find-unused-definitions)
-(define-key emacs-lisp-mode-map (kbd "C-c o o") 'orgtrello-tests-org-raw-coverage)
 
 (provide 'load-org-trello)
 ;;; load-org-trello.el ends here
