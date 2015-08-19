271aa80a312103e3f936d2acaf721e42f8f2ad3f
Antoine R. Dumont <eniotna.t@gmail.com>
2015-08-19 09:36:03 +0100

    Remove the local variable for the moment
    tests need some rewritten before that

--- org-trello-setup.el
+++ org-trello-setup.el
@@ -148,7 +148,9 @@ slightly different than the one from the buffer."
         org-trello--hmap-users-name-id
         org-trello--user-logged-in
         org-trello--mode-activated-p
-        orgtrello-setup-use-position-in-checksum-computation))
+        ;; orgtrello-setup-use-position-in-checksum-computation ;; -> should ideally be that way, need to fix tests before that
+        ))
+
 
 (defconst org-trello--old-config-dir "~/.trello"
   "Old default trello directory.
