231c5c071edd45c03ddb5917c0b2314caa9621d5
Antoine R. Dumont <eniotna.t@gmail.com>
2015-08-24 23:39:05 +0100

    Update stats

--- test/org-trello-input-test.el
+++ test/org-trello-input-test.el
@@ -5,3 +5,9 @@
                  (with-mock
                    (mock (read-string "prompt: ") => :something)
                    (orgtrello-input-read-not-empty "prompt: ")))))
+
+(ert-deftest test-orgtrello-input-read-string ()
+  (should (equal :something
+                 (with-mock
+                   (mock (read-string "prompt: ") => :something)
+                   (orgtrello-input-read-string "prompt: ")))))
