52139497d9b028fd75e81d311fcd9cb216dbe89a
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-11-22 14:33:21 +0100

    ci: Remove helm dependency

--- test/org-trello-input-test.el
+++ test/org-trello-input-test.el
@@ -1,17 +1,19 @@
 (require 'org-trello-input)
 
-(ert-deftest test-orgtrello-input-read-string-completion ()
+(ert-deftest test-orgtrello-input-ido-read-string-completion ()
   (should (eq :res-with-ido
               (let ((org-trello-input-completion-mechanism 'default))
                 (with-mock
                   (mock (ido-completing-read :prompt :choices nil 'do-match) => :res-with-ido)
-                  (orgtrello-input-read-string-completion :prompt :choices)))))
-  ;; cask declares helm as test dependency
-  (should (eq :res-with-helm
-              (let ((org-trello-input-completion-mechanism 'other))
-                (with-mock
-                  (mock (helm-comp-read :prompt :choices) => :res-with-helm)
                   (orgtrello-input-read-string-completion :prompt :choices))))))
+
+(if (boundp 'helm-comp-read)
+    (ert-deftest test-orgtrello-input-helm-read-string-completion ()
+      (should (eq :res-with-helm
+                  (let ((org-trello-input-completion-mechanism 'other))
+                    (with-mock
+                      (mock (helm-comp-read :prompt :choices) => :res-with-helm)
+                      (orgtrello-input-read-string-completion :prompt :choices)))))))
 
 (ert-deftest test-orgtrello-input-read-not-empty ()
   (should (equal "something"
