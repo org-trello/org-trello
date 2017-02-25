da6505b5285ba2352628d18cc78b5c10ac403f88
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-02-25 20:30:56 +0000

    - [X] Upgrade version

--- org-trello-pkg.el
+++ org-trello-pkg.el
@@ -1,7 +1,6 @@
-(define-package "org-trello" "0.7.9" "Minor mode to synchronize org-mode buffer and trello board"
+(define-package "org-trello" "0.8.0" "Minor mode to synchronize org-mode buffer and trello board"
   '((request-deferred "0.2.0")
     (deferred "0.4.0")
     (s "1.11.0")
     (dash-functional "2.12.1")
-    (dash "2.12.1")
-    (emacs "24")))
+    (dash "2.12.1")))
